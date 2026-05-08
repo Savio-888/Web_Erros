feced0869f6316320291ad8a236adef2dfd1e3e1
Savio Pereira Soares <saviopereirasoares6@gmail.com>
2026-05-08 12:07:14 -0300

    segundo commit

--- Aula_0752026/script.js
+++ Aula_0752026/script.js
@@ -1,4 +1,19 @@
-let lista = document.getElementById('listanomes')
+const lista = document.getElementById('listanomes') // Pega a tag de lista no html.
+// código do exercício:
+let convidados = ["Rafaela", "Felipe", "Nicolas", "Amado", "Miguel", "SAIR"];
+let entrada = [];
+let i = 0;
+
 function exibirNome(){
+    while (entrada.length < 5) {
+        const novoConvid = document.createElement('li') // Cria um objeto que simula um elemento dentro do html
+        novoConvid.textContent = convidados[i];
+        if (convidados[i] == "SAIR") {
+            break;
+        }
+        lista.appendChild(novoConvid)
+        entrada.push(convidados[i]);
+        i++;
+    }
     
 }
